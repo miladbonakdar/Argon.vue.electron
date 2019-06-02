21e0a37c200178c9a955ea4d9c9b575180733d1d
milad bonakdar <miladbonak@gmail.com>
2019-06-02 16:01:15 +0430

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <notifications></notifications>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'argon.vue.electron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
